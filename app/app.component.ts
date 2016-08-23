67a537e73c5b4d6e141ee171a2f82ffd43f02342
fscbest <romangeyber@gmail.com>
2016-08-23 11:52:10 +0300

    Add Jumbotron, Home and About

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 
 import {NavbarComponent} from './components/navbar/navbar.component';
+import {JambotronComponent} from './components/jumbotron/jumbotron.component';
+import {HomeComponent} from './components/pages/home.component'
+import {AboutComponent} from './components/pages/about.component'
 
 @Component({
   selector: 'my-app',
-  template: '<navbar></navbar>',
-  directives: [NavbarComponent]
+  template: `<navbar></navbar>
+  <jumbotron></jumbotron>
+  <div class="container">
+    <home></home>
+  </div>`,
+  directives: [NavbarComponent, JambotronComponent, HomeComponent, AboutComponent]
 })
 
 export class AppComponent { }
